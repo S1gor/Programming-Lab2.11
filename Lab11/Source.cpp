a5542275d673adc2d2a25254e141f742510429ef
Igor Semikin <i.semikin@list.ru>
2022-04-06 12:39:25 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Source.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string.h>
+
+//ЗАДАНИЕ:
+//1.Определить, сколько раз в тексте встречается заданная буква.
+//10.Выяснить, верно ли, что каждое слово, не являющее палиндромом, имеет четную длину.
+//19.Даны два слова.Найти только те символы слов, которые встречаются в обоих словах только один раз.
+
+int askUserNumberTask()
+{
+	int choice;
+	do {
+		printf("Введите номер задания (1, 10, 19):\nВыбор:"); scanf_s("%d%*c", &choice);
+	} while (choice != 1 && choice != 10 && choice != 19);
+	return choice;
+}
+
+int main()
+{
+	system("chcp 1251"); system("cls");
+
+	switch (askUserNumberTask())
+	{
+	case 1:
+	{
+		char str[256];
+		printf("Введите строку:\n"); gets_s(str);
+
+		char* search_char = (char*)memchr(str, 'п', strlen(str));
+		break;
+	}
+	case 10:
+	{
+
+		break;
+	}
+	case 19:
+	{
+
+		break;
+	}
+	}
+
+
+
+	return 0;
+}
